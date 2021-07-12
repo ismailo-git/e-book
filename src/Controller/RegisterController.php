95b2bf0e05fecc7977abe4aa865dc1bcf62331da
ismail <hassanliamsi@gmail.com>
2021-07-12 14:28:49 +0200

    Creation du formulaire d'inscription

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class RegisterController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/register", name="register")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
+        ]);
     }
 }
