021c390041542520d2c1699154cd591e34b09268
ismail <hassanliamsi@gmail.com>
2021-07-13 01:26:46 +0200

    Modification templates pour les produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractController
-{
-    #[Route('/product', name: 'product')]
-    public function index(): Response
-    {
+{    private $entityManager;
+
+     public function __construct(EntityManagerInterface $entityManager)
+     {
+         $this->entityManager = $entityManager
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index()
+    {   
+        
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+
+            'products' => $products
         ]);
     }
 }
