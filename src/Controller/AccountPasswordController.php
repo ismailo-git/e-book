e5be3417ea5f7769254b60799fe4e90a3ea3a375
ismail <hassanliamsi@gmail.com>
2021-07-12 21:57:58 +0200

    ye

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AccountPasswordController extends AbstractController
-{
+{   
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+    
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/account/password", name="account_password")
      */
-    public function index(): Response
-    {
-        return $this->render('account/password.html.twig');
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {   
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            
+            $old_pwd = $form->get('old_password')->getData();
+            
+            if ($encoder->isPasswordValid($user, $old_pwd)) {
+                $new_pwd = $form->get('new_password')->getData();
+                 $password = $encoder->encodePassword($user, $new_pwd);
+
+                $user->getPassword($password);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+            }
+
+        }
+        return $this->render('account/password.html.twig', [
+
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -2,6 +2,12 @@
 
 namespace App\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,11 +18,54 @@ class ChangePasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email', EmailType::class, [
+
+                'attr' => [
+                    "class" => "input mt-3"
+                ]
+            
+            ])
+            ->add('firstname', TextType::class, [
+
+                'attr' => [
+                    "class" => "input mt-3"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+
+                'attr' => [
+
+                    "class" => "input mt-3"
+                ]
+            ])
+            ->add('old_password', PasswordType::class, [
+                "mapped" => false,
+                "label" => "Mot de passe actuel",
+                'attr' => [
+
+                    "class" => "input mt-3"
+                ]
+            ])
+             ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => "Les mots de passe ne sont pas identiques",
+                'label' => "Nouveau mot de passe",
+                'required'=> true,
+                "first_options" => ['label' => '    Nouveau Mot de passe', "attr" => ["class" => "input mt-3"]],
+                "second_options" => ['label' => "Confirmez votre mot de passe", "attr" => ["class" => "input mt-3"]],
+                // 'attr' => [
+                //     "placeholder" => "Votre mot de passe",
+                //     "class" => "input"
+                // ]
+            ])
+             ->add('submit', SubmitType::class, [
+                "label" => "Sauvegarder",
+                'attr' => [
+
+                    "class" => "button is-link mt-5"
+                ]
+            ])
         ;
     }
 
