993004156f89011e61fce78538238be5662d5af4
ismail <hassanliamsi@gmail.com>
2021-07-12 18:51:17 +0200

    creation passwordupdate

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccountController extends AbstractController
+class AccountPasswordController extends AbstractController
 {
     /**
-     * @Route("/account", name="account")
+     * @Route("/account/password", name="account_password")
      */
     public function index(): Response
     {
-        return $this->render('account/index.html.twig');
+        return $this->render('account/password.html.twig');
     }
 }
